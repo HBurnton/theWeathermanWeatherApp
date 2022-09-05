d6dd87a25ee30f6677e6617bdf6bf6a7222cddd9
hburnton <hburnton@gmail.com>
2022-09-05 07:56:32 -0700

    add coments and minor restructure

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,11 +8,10 @@ var h2Header = document.querySelector('#fiveDay')
 var priorSearchList = document.querySelector('#priorSearchList')
 var priorSearches = [];
 
-
+//trims entered text from input field and sends it to OpenAPIs GeoCall
 function buttonHandler(event){
     event.preventDefault();
-    city = cityInput.value.trim().toUpperCase();
-
+    city = cityInput.value.trim().toLowerCase();
     if(city == ''){
         alert('Text field is Blank');
         return
@@ -21,11 +20,13 @@ function buttonHandler(event){
     
 }
 
+//retrieves inner text from clicked li and passes as city to GeoCall
 function linkHandler(event){
     city = event.target.textContent;
     getLatLong(city);
 }
 
+//function clears all current html content of fields of containers to be rewritten
 function clearAll(){
     cardContainer.innerHTML = '';
     h2Header.textContent = '';
@@ -35,15 +36,7 @@ function clearAll(){
 
 function getLatLong(city){
 
-    clearAll();
     var APIGeoCall = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`
-
-    if(!priorSearches.includes(city)){
-        priorSearches.push(city);
-        window.localStorage.setItem('storedSearches', JSON.stringify(priorSearches));
-        printPriorSearches();
-    }
-
 
     fetch(APIGeoCall)
         .then(function (response){
@@ -51,6 +44,13 @@ function getLatLong(city){
         })
         .then(function (data){
             if (data.length != 0){
+             //checks waits to see if valid city, if city is valid, then adds to array of cities if new
+                if(!priorSearches.includes(city)){
+                    priorSearches.push(city);
+                    window.localStorage.setItem('storedSearches', JSON.stringify(priorSearches));
+                    printPriorSearches();
+                }
+            //uses retrieved lat and long to call OneCall
             makeOneCall(data[0].lat, data[0].lon);
         }
         })
@@ -66,13 +66,14 @@ function makeOneCall(lat,lon){
             return response.json();
         })
         .then(function (data){
-            console.log(data.current);
-            displayCurrentData(data.current);
+            clearAll();
+            //Uses Data from One Call and sends relevant data to functions to print 5 day forecast and current weather
+            displayCurrentData(data.current, data.daily);
             displayFiveForecast(data.daily);
-
         })
 }
 
+//create individual cards for next 5 days and appends them to cardContainer Div
 function displayFiveForecast(data){
     h2Header.textContent = '5-Day Forecast';
     for(i=1; i<6; i++){
@@ -82,7 +83,7 @@ function displayFiveForecast(data){
                             <h5 class="card-header">${convertDate(data[i].dt)}</h5>
                             <img src=http://openweathermap.org/img/wn/${data[i].weather[0].icon}@2x.png />
                             <ul class="card-text list-unstyled">
-                                <li>Max Temperature: ${data[i].temp.max} F</li>
+                                <li>Max Temperature: ${data[i].temp.max} &degF</li>
                                 <li>Wind Speed: ${data[i].wind_speed} MPH</li>
                                 <li>Humidity: ${data[i].humidity}%</li>
                             </ul>`
@@ -90,7 +91,8 @@ function displayFiveForecast(data){
     }
 }
 
-function displayCurrentData(data){
+//Creates a box to display current temperature and appends to current weather container
+function displayCurrentData(data, todaysHigh){
     var cityCurrentInfo = document.createElement('div');
     var uvColor = getUVColor(data.uvi);
     cityCurrentInfo.setAttribute('class', 'w-100')
@@ -100,7 +102,8 @@ function displayCurrentData(data){
                                  </div>
                                  <h3 class='text-white'>Current Conditions</h3>
                                     <ul class="list-unstyled list-group">
-                                        <li class="list-group-item">Current Temperature: ${data.temp}</li>
+                                        <li class="list-group-item">Current Temperature: ${data.temp} &degF</li>
+                                        <li class="list-group-item">Today's Projected High: ${todaysHigh[0].temp.max} &degF</li>
                                         <li class="list-group-item">Wind Speed: ${data.wind_speed} MPH</li>
                                         <li class="list-group-item">Humidity: ${data.humidity}%</li>
                                         <li class="list-group-item">UV Index: <span class="uvIcon" id='${uvColor}'>${data.uvi}</span></li>
@@ -109,7 +112,9 @@ function displayCurrentData(data){
 
 }
 
+//simple function that returns a string to corresponding ID that styles number with desired color
 function getUVColor(uvIndex){
+
     if(uvIndex < 2){
         return 'low';
     }
@@ -121,11 +126,13 @@ function getUVColor(uvIndex){
     }
 }
 
+//utilized to convert OpenWeather's Native Unix timestamps
 function convertDate(unixDate){
     return moment.unix(unixDate).format("MM/DD/YYYY");
 }
 
-function loadPriorSearches(){
+//checks to see if there are any prior stored searches, if so, sends to below function to create and append list items
+function loadPriorSearches(){ 
     var storedSearches = window.localStorage.getItem("storedSearches")
     if(storedSearches){
         priorSearches = JSON.parse(storedSearches);
@@ -134,6 +141,7 @@ function loadPriorSearches(){
 }
 
 function printPriorSearches(){
+    //
     priorSearchList.innerHTML = '';
     for(var i = 0; i < priorSearches.length; i++){
             var searchLocationLink = document.createElement('li');
@@ -142,7 +150,7 @@ function printPriorSearches(){
     }
 }
 
-//loads everything and adds respective handlers
+//loads prior search history and adds respective handlers
 loadPriorSearches();
 button.addEventListener('click', buttonHandler)
 priorSearchList.addEventListener('click', linkHandler)
